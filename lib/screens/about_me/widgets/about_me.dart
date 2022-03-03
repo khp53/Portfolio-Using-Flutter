a376e6e30507d5d835acfb95506b7a4ecc8b3169
KHP53 <khp4121@gmail.com>
2022-03-03 14:53:04 +0600

    fixed about me text overflow

--- lib/screens/about_me/widgets/about_me.dart
+++ lib/screens/about_me/widgets/about_me.dart
@@ -31,9 +31,12 @@ class AboutMe extends StatelessWidget {
                   SizedBox(
                     height: 35,
                   ),
-                  Text(
-                    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\nLorem ipsum dolor sit amet, consetetur",
-                    style: _theme.textTheme.bodyText1,
+                  Container(
+                    height: 500,
+                    child: Text(
+                      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\nLorem ipsum dolor sit amet, consetetur",
+                      style: _theme.textTheme.bodyText1,
+                    ),
                   ),
                 ],
               ),
@@ -46,7 +49,7 @@ class AboutMe extends StatelessWidget {
                 color: Colors.black,
                 image: DecorationImage(
                   image: AssetImage('assets/images/me.png'),
-                  fit: BoxFit.fill,
+                  fit: BoxFit.contain,
                 ),
               ),
             ),
