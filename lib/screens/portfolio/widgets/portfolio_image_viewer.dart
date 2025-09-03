8e8606adb8cd07654c0259e5bccbe1f913f5a12f
khp53 <khp4121@gmail.com>
2025-09-03 20:42:51 -0230

    refactor: hide CarouselController import in portfolio_image_viewer

--- lib/screens/portfolio/widgets/portfolio_image_viewer.dart
+++ lib/screens/portfolio/widgets/portfolio_image_viewer.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide CarouselController;
 import 'package:portfolio/commons/is_mobileCall.dart';
 import 'package:portfolio/models/portfolio_dev.dart';
 
